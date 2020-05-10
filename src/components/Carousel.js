86352ee3b3e223494ec3711253067cbdb3085d29
otis <otis1234@hotmail.co.uk>
2020-05-10 20:14:21 +0100

    second commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/Carousel.css";
+import "../css/Grid.css";
 import StaffParty1 from "../imgs/carousel/staff4.jpg";
 
 const Carousel = () => {
   return (
     <div className="carouselContainer">
-      <img alt="#" src={StaffParty1} />
+      <img className="coverPic" alt="#" src={StaffParty1} />
+      <span className="headerText">STAFF PARTY</span>
     </div>
   );
 };
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,15 +1,30 @@
 import React, { Fragment } from "react";
 import "../css/Grid.css";
+import "../css/Grid.css";
+import StaffParty1 from "../imgs/carousel/staff4.jpg";
+import StaffParty2 from "../imgs/carousel/kingjohnno1.jpg";
+import StaffParty3 from "../imgs/carousel/kingned1.jpg";
+import StaffParty4 from "../imgs/carousel/kingned4.jpg";
 
 const Grid = () => {
   return (
     <Fragment>
-      <div className="gridContainer">
-        <div className="gridItem"></div>
-        <div className="gridItem"></div>
-        <div className="gridItem"></div>
-        <div className="gridItem"></div>
-        <span className="headerText">STAFF PARTY</span>
+      <div>
+        <div className="gridContainer">
+          <div className="gridItem">
+            <img alt="staffparty1" src={StaffParty1} />
+          </div>
+          <div className="gridItem">
+            <img alt="staffparty2" src={StaffParty2} />
+          </div>
+          <div className="gridItem">
+            <img alt="staffparty3" src={StaffParty3} />
+          </div>
+          <div className="gridItem">
+            <img alt="staffparty4" src={StaffParty4} />
+          </div>
+          <span className="headerText">STAFF PARTY</span>
+        </div>
       </div>
     </Fragment>
   );
