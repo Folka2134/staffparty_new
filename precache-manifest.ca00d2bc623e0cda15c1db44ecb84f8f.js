84b18e7afd8dbccadc2901f14d3aeaa5b16bbfc5
otis <otis1234@hotmail.co.uk>
2020-05-11 21:09:09 +0100

    Updates

---
+++ precache-manifest.ca00d2bc623e0cda15c1db44ecb84f8f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c393a414b418529fef582267f9ea10",
+    "url": "/staffparty_new/index.html"
+  },
+  {
+    "revision": "a6e7a90f6fe1113cfaa9",
+    "url": "/staffparty_new/static/css/main.890d2dfd.chunk.css"
+  },
+  {
+    "revision": "f27b489a604d083985ae",
+    "url": "/staffparty_new/static/js/2.5b591e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staffparty_new/static/js/2.5b591e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e7a90f6fe1113cfaa9",
+    "url": "/staffparty_new/static/js/main.92df4ef4.chunk.js"
+  },
+  {
+    "revision": "14dde945351f9ce2fd8d",
+    "url": "/staffparty_new/static/js/runtime-main.e16c3ca2.js"
+  },
+  {
+    "revision": "0e05f4cef6427093b38fc797bbe3fa6c",
+    "url": "/staffparty_new/static/media/Blacklisted.0e05f4ce.ttf"
+  },
+  {
+    "revision": "a2e777184b9ac6495052de6151bbef4e",
+    "url": "/staffparty_new/static/media/concert8.a2e77718.jpg"
+  },
+  {
+    "revision": "0754a87a6aa14ab86abe03e805d71adf",
+    "url": "/staffparty_new/static/media/coolcon.0754a87a.ttf"
+  },
+  {
+    "revision": "2080384f31921023ebec42f9420f1357",
+    "url": "/staffparty_new/static/media/kingjohnno1.2080384f.jpg"
+  },
+  {
+    "revision": "014522f89549e202655bb890fdac4919",
+    "url": "/staffparty_new/static/media/kingned1.014522f8.jpg"
+  },
+  {
+    "revision": "3bfd473a08f5b65af8e2e37bb83953a4",
+    "url": "/staffparty_new/static/media/kingned4.3bfd473a.jpg"
+  },
+  {
+    "revision": "6ff2da68af54d0e1183a2aea553d1144",
+    "url": "/staffparty_new/static/media/staff4.6ff2da68.jpg"
+  }
+]);
